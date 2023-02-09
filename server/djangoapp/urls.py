61d5ab6470f994c08d8f104a353cf66afeb23e24
Xin Zheng <zsen86496790@gmail.com>
2023-02-09 16:56:18 -0500

    day 1

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -9,9 +9,11 @@ urlpatterns = [
     # view refers to the view function
     # name the URL
 
-    # path for about view
+     # path for about view
+    path(route='about', view=views.about, name='about'),
 
     # path for contact us view
+    path(route='contact', view=views.contact, name='contact'),
 
     # path for registration
 
